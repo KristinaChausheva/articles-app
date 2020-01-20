aa2b5e547a4ef98c5b809dac6ff46d18e22f9698
Kristina Chausheva <k.chausheva@gmail.com>
2020-01-20 02:51:43 +0200

    updates

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,50 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Article } from '../models/article.model';
+import { OnInit } from '@angular/core';
+import { Article } from "../models/article.model";
+import { Component, Input } from '@angular/core';
+
 
 @Component({
-  selector: 'app-articles',
-  templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.css']
-})
-export class ArticleComponent implements OnInit {
+    selector: 'app-articles',
+    templateUrl: './articles.component.html',
+    styleUrls: ['./articles.component.css']
+  })
 
-  private symbols: number = 250;
-  @Input() article: Article;
-  @Input() articleDesc: string;
-  descToShow: string;
-  articleDescLen: number;
-  showReadMoreBtn: boolean = true;
-  showHideBtn: boolean = false;
-  imageIsShown: boolean = false;
-  imageButtonTitle: string = "Show Image";
-  constructor() { 
-    this.articleDescLen = 0;
-    this.descToShow = "";
-  }
+export class ArticlesComponent implements OnInit {
+    articles: Article[];
 
-  readMore() :void {
-    this.articleDescLen += this.symbols;
-    if (this.articleDescLen >= this.articleDesc.length) {
-      this.showHideBtn = true;
-      this.showReadMoreBtn = false;
-    } else {
-      this.descToShow = this.articleDesc.substring(0, this.articleDescLen);
+    constructor() { }
+
+    ngOnInit() {
+        this.articles;
+
     }
-  }
-
-  toggleImage(): void {
-    this.imageIsShown = !this.imageIsShown;
-    this.imageButtonTitle = this.imageButtonTitle === "Show Image" ? "Hide Image" : "Show image";
-  }
-
-  hideDesc() : void {
-    this.descToShow = "";
-    this.articleDescLen = 0;
-    this.showHideBtn = false;
-    this.showReadMoreBtn = true;
-  }
-
-  ngOnInit() {
-  }
-
 }
